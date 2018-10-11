38d648a72e9d4ebdca8967618b2749dea38a8502
Steven Oud <soud@protonmail.com>
2018-10-11 20:45:30 +0200

    Don't use var args as optional value

--- src/basically/errors.clj
+++ src/basically/errors.clj
@@ -1,14 +1,16 @@
 (ns basically.errors)
 
-(defn error [type & [label]]
-  (let [message (case type
-                  :type-mismatch "?TYPE MISMATCH ERROR"
-                  :syntax-error "?SYNTAX ERROR"
-                  :undefd-statement "?UNDEF'D STATEMENT ERROR"
-                  :next-without-for "?NEXT WITHOUT FOR ERROR"
-                  :return-without-gosub "?RETURN WITHOUT GOSUB ERROR"
-                  :undefd-function "?UNDEF'D FUNCTION ERROR")]
-    (throw (Exception. (str message (when label (str " IN " label)))))))
+(defn error
+  ([type] (error type nil))
+  ([type label]
+   (let [message (case type
+                   :type-mismatch "?TYPE MISMATCH ERROR"
+                   :syntax-error "?SYNTAX ERROR"
+                   :undefd-statement "?UNDEF'D STATEMENT ERROR"
+                   :next-without-for "?NEXT WITHOUT FOR ERROR"
+                   :return-without-gosub "?RETURN WITHOUT GOSUB ERROR"
+                   :undefd-function "?UNDEF'D FUNCTION ERROR")]
+     (throw (Exception. (str message (when label (str " IN " label))))))))
 
 (defn error-with-mem [type mem]
   (let [label (:current-label @mem)]
